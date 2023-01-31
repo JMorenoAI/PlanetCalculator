14b38fc7a79e3c5f80ccc2f3e76715160ec4f2af
Joshua Moreno <116939597+JMorenoAI@users.noreply.github.com>
2023-01-31 10:45:44 -0600

    Codes for Planet Calculator

---
+++ PlanetCalculatorCode.py
@@ -0,0 +1,28 @@
+planets = (
+    ('Mercury', 57),
+    ('Venus', 108),
+    ('Earth', 150),
+    ('Mars', 228),
+    ('Jupiter', 779),
+    ('Saturn', 1430),
+    ('Uranus', 2880),
+    ('Neptune', 4500)
+
+)
+
+print("============================================================")
+print("Planet's Average Distance From Sun")
+print("============================================================")
+print("#1 Mercury =   57 million miles \n"
+      "#2 Venus   =  108 million miles \n"
+      "#3 Earth   =  150 million miles \n"
+      "#4 Mars    =  228 million miles \n"
+      "#5 Jupiter =  779 million miles \n"
+      "#6 Saturn  = 1430 million miles \n"
+      "#7 Uranus  = 2880 million miles \n"
+      "#8 Neptune = 4500 million miles \n")
+print("============================================================")
+print("To calculate the distance between two planets \n"
+      "Enter two planet numbers or 0 to quit: \n")
+print("------------------------------------------------------------")
+
