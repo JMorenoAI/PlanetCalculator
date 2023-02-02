66961fbc3eee4acc7ec973ba4a0341d29155ade1
CTIS <codygreenwood1993@gmail.com>
2023-02-02 10:01:21 -0600

    Change the first display to match the requirements of the assignment

--- PlanetCalculatorCode.py
+++ PlanetCalculatorCode.py
@@ -1,3 +1,7 @@
+numbers = 0
+column_length = 9
+number_length = 5
+
 planets = (
     ('Mercury', 57),
     ('Venus', 108),
@@ -13,17 +17,11 @@ planets = (
 print("============================================================")
 print("Planet's Average Distance From Sun")
 print("============================================================")
-print("#1 Mercury =   57 million miles \n"
-      "#2 Venus   =  108 million miles \n"
-      "#3 Earth   =  150 million miles \n"
-      "#4 Mars    =  228 million miles \n"
-      "#5 Jupiter =  779 million miles \n"
-      "#6 Saturn  = 1430 million miles \n"
-      "#7 Uranus  = 2880 million miles \n"
-      "#8 Neptune = 4500 million miles \n")
+for planet, distance in planets:
+    numbers += 1
+    print("#" + str(numbers) + " " + planet + " = " + str(distance) + " million miles")
+
 print("============================================================")
 print("To calculate the distance between two planets \n"
       "Enter two planet numbers or 0 to quit: \n")
 print("------------------------------------------------------------")
-
-
